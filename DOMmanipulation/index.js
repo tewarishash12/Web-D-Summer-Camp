0d1c3bd0a61ff620f967252be9a930236a254221
tewarishash12 <tewarishashwat2002@gmail.com>
2024-07-24 22:22:18 +0530

    Summer Camp Practice Code

---
+++ DOMmanipulation/index.js
@@ -0,0 +1,71 @@
+const body = document.body;
+
+const container = document.getElementById('container');
+container.style.display = 'flex';
+container.style.justifyContent="center";
+container.style.alignItems="center";
+container.style.height="80vh";
+container.style.gap = "5px";
+
+const black = document.createElement('div');
+black.className = "color";
+black.style.backgroundColor = "black";
+black.classList.add('black');
+container.appendChild(black);
+
+const red = document.createElement('div');
+red.className = "color";
+red.style.backgroundColor = "red";
+black.classList.add('red');
+container.appendChild(red);
+
+const green = document.createElement('div');
+green.className = "color";
+green.style.backgroundColor = "green";
+black.classList.add('green');
+container.appendChild(green);
+
+const yellow = document.createElement('div');
+yellow.className = "color";
+yellow.style.backgroundColor = "yellow";
+black.classList.add('yellow');
+container.appendChild(yellow);
+
+let colors = document.querySelectorAll('.color');
+
+colors.forEach(boxes => {
+    boxes.style.width = "100px";
+    boxes.style.height = "100px";
+});
+
+black.addEventListener('click', function(){
+    body.style.backgroundColor = "black";
+    black.style.border = "2px solid white";
+    red.style.border = "none";
+    green.style.border = "none";
+    yellow.style.border = "none";
+});
+
+red.addEventListener('click', function(){
+    body.style.backgroundColor = "red";
+    red.style.border = "2px solid white";
+    black.style.border = "none";
+    green.style.border = "none";
+    yellow.style.border = "none";
+});
+
+green.addEventListener('click', function(){
+    body.style.backgroundColor = "green";
+    green.style.border = "2px solid white";
+    red.style.border = "none";
+    black.style.border = "none";
+    yellow.style.border = "none";
+});
+
+yellow.addEventListener('click', function(){
+    body.style.backgroundColor = "yellow";
+    yellow.style.border = "2px solid white";
+    red.style.border = "none";
+    green.style.border = "none";
+    black.style.border = "none";
+});
--- loops.js
+++ loops.js
@@ -13,9 +13,59 @@ let i=5;
 //     console.log(arr[i]);
 // }
 
-i=0;
+// i=0;
 
-while(i<10){
-    console.log(`loop is on ${i}`);
-    i++;
-}
+// while(i<10){
+//     console.log(`loop is on ${i}`);
+//     i++;
+// }
+
+const todo = [
+    {
+        id: 1,
+        text:"coding",
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text:"reading",
+        isCompleted: false
+    },
+    {
+        id: 1,
+        text:"cycling",
+        isCompleted: true
+    },
+];
+
+// todo.forEach(function(todo){
+//     console.log(`Task: ${todo.text}, Status: ${todo.isCompleted}`)
+// })
+
+// const status = todo.forEach(item => ({
+//     return (`Task: ${todo.text}, Status: ${todo.isCompleted}`);
+// }))
+
+const year = 1972;
+
+// if(year%4===0){
+//     if(year%100===0){
+//         if(year%400===0){
+//             console.log(`${year} is a leap year`);
+//         }else{
+//             console.log(`${year} is not a leap year`);
+//         }
+//     } else{
+//         console.log(`${year} is a leap year`);
+//     }
+// }
+// else
+// console.log(`${year} is not a leap year`);
+
+
+if(year%4===0 && year%100==0 && year%400===0)
+    console.log(`${year} is a leap year`);
+else if(year%4===0 && year%100!==0)
+    console.log(`${year} is a leap year`);
+else
+    console.log(`${year} is not a leap year`);
